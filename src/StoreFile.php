06dfa93f6bd99d761aa56e325889f2de9b68a5eb
Yeray Navarro <yeraynp16@gmail.com>
2024-11-04 17:16:00 +0000

    save method

--- src/StoreFile.php
+++ src/StoreFile.php
@@ -1,9 +1,14 @@
 <?php
 namespace Daw2\DswRating;
 
+use DateTime;
+
 class StoreFile implements StoreInterface {
   public function addRate(int $rate) {
-    
+    $date = new DateTime();
+    $path = '../data/';
+    $fileName = $path . $date->format('Y_m_d_H_i') . 'csv';
+    file_put_contents($fileName, $rate . ',', FILE_APPEND);
   }
 
   public function getStadistics(): array {
